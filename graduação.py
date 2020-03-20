e783f48dbb0ce0c67f1dc1e1f6d27a45eb1e045b
4liss0n <61910302+4liss0n@users.noreply.github.com>
2020-03-20 10:06:45 -0300

    Add files via upload

---
+++ graduação.py
@@ -0,0 +1,5 @@
+graduacao = input(print("qual o seu nivel de graduação?Tecnologo ou baicharelado?")).upper()
+if graduacao == "TECNOLOGO":
+    print("vai levar de dois a três anos, certo?")
+elif graduacao == "BAICHARELADO":
+    print("de quatro a seis anos, certo?")
